803331246f6604c0fdbb934bc0bfbd5084aa72f8
serhanelmali <serhanelmali@gmail.com>
2021-09-23 17:20:25 +0300

    feat(target-client): adds target-client

--- app.js
+++ app.js
@@ -6,11 +6,15 @@ require("dotenv").config();
 const port = process.env.PORT || 7000;
 
 const metricRouter = require("./routes/metric");
+const scriptRouter = require("./routes/script");
+const targetRouter = require("./routes/target");
 
 db();
 app.use(morgan("tiny"));
 app.use(express.json());
 
 app.use("/metric", metricRouter);
+app.use("/script", scriptRouter);
+app.use("/target", targetRouter);
 
 app.listen(port, () => console.log(`Server is running on port ${port}`));
